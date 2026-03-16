9e1586a8b6b222a89f2370e3bd1ff99bf7b6d836
SourceDevv <modulescript2k@gmail.com>
2026-03-16 18:15:25 -0500

    fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/CrosswordLengualab',
+  base: '/Lengualab',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
